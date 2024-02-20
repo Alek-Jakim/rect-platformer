0429b6d7a7c7bda19d780f9a1dc8a3fb1c379d62
Alek <alek.jakimovski1@gmail.com>
2024-02-20 19:49:41 +0100

    implemented new platforms

--- assets/level/level_one.py
+++ assets/level/level_one.py
@@ -5,5 +5,9 @@ map_one = {
     "floor": [(WIN_WIDTH, 75), (0, WIN_HEIGHT - 75), "blue"],
     "wall_left": [(50, WIN_HEIGHT), (0, 0), "blue"],
     "wall_right": [(50, WIN_HEIGHT), (WIN_WIDTH - 50, 0), "blue"],
-    "platform_one": [(100, 50), (500, 500), "blue"],
+    "platform_one": [(400, 25), (50, 450), "blue"],
+    "platform_two": [(400, 25), (WIN_WIDTH - 450, 450), "blue"],
+    "platform_three": [(300, 25), (50, 300), "blue"],
+    "platform_four": [(300, 25), (WIN_WIDTH - 350, 300), "blue"],
+    "platform_five": [(300, 25), (WIN_WIDTH - 250, 150), "blue"],
 }
